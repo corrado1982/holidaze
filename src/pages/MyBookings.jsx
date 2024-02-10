7618864f83296c1374932d13f2ff559e91298d97
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-10 01:27:09 +0100

    created mybooking page

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,5 +1,79 @@
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/api";
+import * as storage from "../storage/index";
+const user = storage.load("username");
+const url =
+  BASE_URL + "/profiles/" + user + "/bookings?_customer=true&_venue=true";
+
+const token = storage.load("token");
+
 function MyBookings() {
-  return <h1>Bookings page</h1>;
+  const [posts, setPosts] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsError(false);
+
+        setIsLoading(true);
+        const response = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          method: "GET",
+        });
+
+        const json = await response.json();
+        setPosts(json);
+        // console.log(json);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+
+    getData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading posts</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading data</div>;
+  }
+  console.log(posts);
+
+  return (
+    <>
+      <h1>My Bookings</h1>
+      <div className="bg-sky-50 rounded-lg shadow-xl size-4/5 m-auto">
+        {posts.map((myBooking) => (
+          <div className="my-5 card-bg mx-auto flex" key={myBooking.id}>
+            <img
+              src={myBooking.venue.media[0]}
+              alt=""
+              className=" h-24 w-24 object-fill rounded-lg m-2"
+            />
+            <div className="flex flex-col justify-between">
+              <div>
+                <h2>{myBooking.venue.name}</h2>
+              </div>
+              <div className="mb-2">
+                <p>from: {myBooking.dateFrom}</p>
+                <p>to: {myBooking.dateTo}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default MyBookings;
