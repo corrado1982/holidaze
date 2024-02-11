c153555f6d5dc4e8a0b7f25235b693b2bd8906b4
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-11 19:47:26 +0100

    not fixed

--- src/pages/MyBookingsPage.jsx
+++ src/pages/MyBookingsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../constants/api";
 import * as storage from "../storage/index";
 import MyBookings from "../components/MyBookings";
-// import { removePost } from "../components/MyBookings";
+
 const user = storage.load("username");
 const url =
   BASE_URL + "/profiles/" + user + "/bookings?_customer=true&_venue=true";
@@ -12,6 +12,8 @@ const token = storage.load("token");
 // const urlRemove = BASE_URL + "/bookings/" + id;
 
 export async function removePost(id) {
+  // const [remove, setRemove] = useState(false);
+
   const urlRemove = BASE_URL + "/bookings/" + id;
   const response = await fetch(urlRemove, {
     headers: {
@@ -22,13 +24,19 @@ export async function removePost(id) {
   });
   console.log(response);
 }
-
+// async function removePost() {
+//   setRemove(true);
+// }
 function MyBookingsPage() {
   const [posts, setPosts] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false);
 
   const [isError, setIsError] = useState(false);
+
+  // const [remove, setRemove] = useState(false);
+  // remove = false;
+
   useEffect(() => {
     async function getData() {
       try {
