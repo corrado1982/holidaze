4c5831236dce99a81f0427e23d81e813727d6dda
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-20 22:56:09 +0100

    removed Link from modal and use modal as feedback

--- src/components/modal/BookingVenueConfirm.jsx
+++ src/components/modal/BookingVenueConfirm.jsx
@@ -49,21 +49,30 @@ function BookingVenueConfirm() {
                       >
                         You have booked successfully
                       </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          For Details go back on "my bookings" page
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <Link to="/mybookings" className=" btn-primary mx-auto my-10">
-                    Ok
-                  </Link>
                   {/* <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button> */}
+                  <button
+                    type="button"
+                    className="btn-primary"
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
-                    Cancel
-                  </button> */}
+                    Ok
+                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
