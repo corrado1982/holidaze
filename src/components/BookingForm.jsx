fa4175b5c95b4e93bf87b179fa40718d26bac319
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-20 18:56:28 +0100

    clean

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, json, useParams } from "react-router-dom";
+import React, { useState } from "react";
+// import { Link, json, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { BASE_URL } from "../constants/api";
