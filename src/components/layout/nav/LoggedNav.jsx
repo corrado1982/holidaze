a9379455f4714a43bf8911ecc9f60d06630d8d6d
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-24 14:01:41 +0100

    fixed kind of "modal" in the nav

--- src/components/layout/nav/LoggedNav.jsx
+++ src/components/layout/nav/LoggedNav.jsx
@@ -5,78 +5,54 @@ import { isManager } from "../../isItLogged";
 import * as storage from "../../../storage/index";
 import logout from "../../../auth/logout";
 import holidazeLogo from "../../../../public/holidaze-logo.png";
-// import logout from "../../../auth/logout";
-
 const userName = storage.load("username");
-const avatarPicture = storage.load("avatar");
 
 function LoggedNav() {
-  // const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-  // if (showMenu === true) {
-  //   document.addEventListener("click", handleClickOut, true);
-  // }
+  function openModal() {
+    if (showMenu === false) {
+      setShowMenu(true);
+    }
+    if (showMenu === true) {
+      setShowMenu(false);
+    }
+  }
 
-  // function handleShowMenu() {
-  //   if (showMenu === false) {
-  //     setShowMenu(true);
-  //   }
+  // HANDLE CLICK OUT
+
+  // function handleClickOut() {
   //   if (showMenu === true) {
   //     setShowMenu(false);
   //   }
   // }
-
-  // function handleClickOut() {
-  //   setShowMenu(false);
+  // if (showMenu === true) {
+  //   document.addEventListener("click", handleClickOut, true);
   // }
+
+  console.log(showMenu);
   return (
-    <nav className="flex">
-      <ul className="relative ml-3 flex justify-between p-2 w-11/12 items-center">
-        <li>
-          <NavLink to="/">
-            <img src={holidazeLogo} alt="logo" className=" w-28" />
-          </NavLink>
-        </li>
-        {/* <div className="flex items-center"> */}
-        {/* <p> */}
-        {/* <img
-            className="h-8 w-8 rounded-full"
-            src={avatarPicture}
-            alt="my avatar"
-          ></img> */}
-        {/* {userName} */}
-        {/* </p> */}
-        {/* <div> */}
-        {/* <button
-              onClick={handleShowMenu}
-              type="button"
-              className="relative flex rounded-full"
-              id="user-menu-button"
-              aria-expanded="false"
-              aria-haspopup="true"
-            >
-              <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full"
-                src={avatarPicture}
-                alt=""
-              ></img>
-            </button> */}
-        {/* </div> */}
-        {/* MENU */}
-        {/* {showMenu ? ( */}
-        {/* <div
-              className="absolute right-0 z-10 mt-44 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="user-menu-button"
-              tabIndex="-1"
-            > */}
-        {/* AVATAR */}
+    <div>
+      <nav className="flex">
+        <ul className="relative ml-3 flex justify-between p-2 w-11/12 items-center">
+          <li>
+            <NavLink to="/">
+              <img src={holidazeLogo} alt="logo" className=" w-28" />
+            </NavLink>
+          </li>
+        </ul>
+
         <div>
+          <button onClick={openModal}>{userName}</button>
+        </div>
+        <div className="flex items-center">
+          <Link onClick={logout}>Log out</Link>
+        </div>
+      </nav>
+      {showMenu ? (
+        <ul className=" absolute right-0 max-w-48  bg-cyan-400">
           <li className="flex">
-            <p>{userName}</p>
+            {/* AVATAR */}
             <NavLink
               to="/avatar"
               className="block px-4 py-2 "
@@ -92,45 +68,39 @@ function LoggedNav() {
               ></img> */}
             </NavLink>
           </li>
-        </div>
 
-        {/* MY BOOKINGS */}
-        <li>
-          <NavLink
-            to="/mybookings"
-            className="block px-4 py-2 "
-            role="menuitem"
-            tabIndex="-1"
-            id="user-menu-item-1"
-          >
-            My Bookings
-          </NavLink>
-        </li>
-
-        {/* MY VENUE */}
-        {isManager() && (
+          {/* MY BOOKINGS */}
           <li>
             <NavLink
+              to="/mybookings"
               className="block px-4 py-2 "
               role="menuitem"
               tabIndex="-1"
-              id="user-menu-item-2"
-              to="/myvenue"
+              id="user-menu-item-1"
             >
-              My venue
+              My Bookings
             </NavLink>
           </li>
-        )}
 
-        {/* LOG OUT */}
-        {/* </div> */}
-        {/* ) : null} */}
-        {/* </div> */}
-      </ul>
-      <div className="flex items-center">
-        <Link onClick={logout}>Log out</Link>
-      </div>
-    </nav>
+          {/* MY VENUE */}
+          {isManager() && (
+            <li>
+              <NavLink
+                className="block px-4 py-2 "
+                role="menuitem"
+                tabIndex="-1"
+                id="user-menu-item-2"
+                to="/myvenue"
+              >
+                My venue
+              </NavLink>
+            </li>
+          )}
+        </ul>
+      ) : (
+        <div></div>
+      )}
+    </div>
   );
 }
 
