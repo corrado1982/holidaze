a64c9e94a7a500df965466aad2e15b1da0474505
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-02-25 13:00:11 +0100

    styled footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <footer>Website footer</footer>;
+  return (
+    <footer className="mx-auto flex flex-col justify-center items-center ">
+      <h2>Holidaze</h2>
+      <p>A webside made with React</p>
+      <p>by Corrado Di Rofi</p>
+      <p>corrado.rofi@gmail.com</p>
+    </footer>
+  );
 }
 
 export default Footer;
